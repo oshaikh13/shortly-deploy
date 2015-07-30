c96bcc8f3432b7f89d349b751c1c4c6e568a1e1b
Hack Reactor Students <accounts@hackreactor.com>
2015-07-29 19:37:35 -0700

    Completed Links in MongoDB

--- app/models/user.js
+++ app/models/user.js
@@ -1,6 +1,4 @@
-var db = require('../config');
-var bcrypt = require('bcrypt-nodejs');
-var Promise = require('bluebird');
+var mongoose = require('../config');
 
 // var User = db.Model.extend({
 //   tableName: 'users',
