a62c64e5791c9d58ed62e2d2b109c6b322063b18
Hack Reactor Students <accounts@hackreactor.com>
2015-07-29 17:14:07 -0700

    Edited more stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,35 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
     concat: {
+      // options: {
+      //   separator: ';',
+      // },
+
+      basic:  {
+        src: ['./public/lib/underscore.js', 
+              './public/lib/handlebars.js', 
+              './public/lib/jquery.js', 
+              './public/lib/backbone.js'
+             ],
+
+        dest: './public/dist/built.js',
+      },
+
+      extras:  {
+        src: ['./public/client/app.js', 
+              './public/client/link.js', 
+              './public/client/links.js', 
+              './public/client/linkView.js',
+              './public/client/linksView.js', 
+              './public/client/createLinkView.js', 
+              './public/client/router.js'
+             ],
+
+        dest: './public/dist/backboneApp.js',
+      }
+
     },
 
     mochaTest: {
@@ -21,24 +49,37 @@ module.exports = function(grunt) {
     },
 
     uglify: {
+      my_target: {
+        files: {
+          './public/dist/built.js': ['./public/dist/built.js'],
+          './public/dist/backboneApp.js': ['./public/dist/backboneApp.js']
+        }
+      }
     },
 
     jshint: {
       files: [
         // Add filespec list here
+        './**/*.js'
       ],
       options: {
         force: 'true',
         jshintrc: '.jshintrc',
         ignores: [
           'public/lib/**/*.js',
-          'public/dist/**/*.js'
+          'public/dist/**/*.js',
+          'node_modules/**/*.js'
         ]
       }
     },
 
     cssmin: {
         // Add filespec list here
+      target: {
+        files: {
+          './public/dist/style.css': ['./public/style.css']
+        }
+      }
     },
 
     watch: {
@@ -72,6 +113,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-purifycss');
 
   grunt.registerTask('server-dev', function (target) {
     // Running nodejs in a different process and displaying output on the main console
@@ -90,24 +132,32 @@ module.exports = function(grunt) {
   // Main grunt tasks
   ////////////////////////////////////////////////////
 
-  grunt.registerTask('test', [
-    'mochaTest'
+  grunt.registerTask('build', [
+    'jshint',
+    'mochaTest',
+    'concat',
+    'uglify',
+    'cssmin'
   ]);
 
-  grunt.registerTask('build', [
+  grunt.registerTask('dev', [
+
   ]);
 
   grunt.registerTask('upload', function(n) {
     if(grunt.option('prod')) {
       // add your production server task here
+      grunt.task.run(['build']);
     } else {
-      grunt.task.run([ 'server-dev' ]);
+      grunt.task.run(['build','server-dev']);
     }
   });
 
   grunt.registerTask('deploy', [
-      // add your production server task here
+      'upload'
   ]);
+
+
 
 
 };
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var app = require('./server-config.js');
 
-var port = process.env.port;
+var port = process.env.port || 8080;
 app.listen(port);
 
 console.log('Server now listening on port ' + port);
